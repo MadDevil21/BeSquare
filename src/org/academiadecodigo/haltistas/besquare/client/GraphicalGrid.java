0e7b826f098e6c92dc221da160e38bab4f08f0a7
Bruno Santos <brunodominguessantos@gmail.com>
2018-03-14 23:46:02 +0000

    cleanup for merge with develop

--- src/org/academiadecodigo/haltistas/besquare/client/GraphicalGrid.java
+++ src/org/academiadecodigo/haltistas/besquare/client/GraphicalGrid.java
@@ -1,9 +1,6 @@
 package org.academiadecodigo.haltistas.besquare.client;
 
-
-import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -21,30 +18,8 @@ public class GraphicalGrid {
         gridCells = new Rectangle[NUM_ROWS][NUM_COLS];
     }
 
-    public static void main(String[] args) {
 
-        GraphicalGrid grid = new GraphicalGrid();
-        grid.createGrid();
-        grid.drawGrid();
-    }
-
-
-    private void createGrid() {
-
-        for (int rows = 0; rows < NUM_ROWS; rows++) {
-            for (int cols = 0; cols < NUM_COLS; cols++) {
-
-                int rowPosition = (PADDING + (CELL_SIZE * rows));
-                int colPosition = (PADDING + (CELL_SIZE * cols));
-
-                gridCells[rows][cols] = new Rectangle(colPosition, rowPosition, CELL_SIZE, CELL_SIZE);
-                gridCells[rows][cols].draw();
-
-            }
-        }
-    }
-
-    public static String load() {
+    public static String loadFile() {
 
         FileReader reader = null;
         String stringToReturn = "";
@@ -56,69 +31,21 @@ public class GraphicalGrid {
 
             stringToReturn += bReader.readLine();
 
-        } catch (IOException ex) {
-
+        } catch(IOException ex) {
             System.err.println(ex.getMessage());
+
         } finally {
-
             try {
-
-                if (reader != null) {
+                if(reader != null) {
                     reader.close();
                 }
-
-            } catch (IOException ex) {
+            } catch(IOException ex) {
                 ex.printStackTrace();
-
             }
         }
         return stringToReturn;
     }
-
-
-    private void drawGrid() {
-
-        String loadedString = load();
-
-        for (int col = 0; col < NUM_COLS; col++) {
-            for (int row = 0; row < NUM_ROWS; row++) {
-                char color = loadedString.charAt(col * NUM_ROWS + row);
-                paintCell(col, row, color);
-
-
-            }
-        }
-
     }
 
 
-    public void paintCell(int col, int row, char color) {
 
-        Rectangle rectangle = gridCells[row][col];
-        rectangle.fill();
-
-        switch (color) {
-
-            case 'i':
-                rectangle.setColor(Color.BLACK);
-                break;
-
-            case 'x':
-                rectangle.setColor(Color.RED);
-                break;
-
-            case 'o':
-                rectangle.setColor(Color.WHITE);
-                break;
-
-            case 'd':
-                rectangle.setColor(Color.GREEN);
-                break;
-
-            default:
-                rectangle.setColor(Color.WHITE);
-
-        }
-
-    }
-}
