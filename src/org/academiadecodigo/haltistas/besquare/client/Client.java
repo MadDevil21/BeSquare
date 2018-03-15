466bfe3522c81f5d2c6684885b0b3832fd34caeb
Bruno Santos <brunodominguessantos@gmail.com>
2018-03-15 22:38:17 +0000

    Implemented controller mockup

--- src/org/academiadecodigo/haltistas/besquare/client/Client.java
+++ src/org/academiadecodigo/haltistas/besquare/client/Client.java
@@ -5,4 +5,11 @@ package org.academiadecodigo.haltistas.besquare.client;
 public class Client {
 
 
+    public void moveRight(){
+
+    }
+
+    public void moveLeft(){
+
+    }
 }
--- src/org/academiadecodigo/haltistas/besquare/client/Controller.java
+++ src/org/academiadecodigo/haltistas/besquare/client/Controller.java
@@ -1,4 +1,56 @@
 package org.academiadecodigo.haltistas.besquare.client;
 
-public class Controller {
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+
+public class Controller implements KeyboardHandler {
+
+    private Client client;
+
+    public Controller(Client client) {
+        this.client = client;
+    }
+
+    public void init(){
+
+        Keyboard keyboard = new Keyboard(this);
+
+        int[] keys = new int[] {
+                KeyboardEvent.KEY_RIGHT,
+                KeyboardEvent.KEY_LEFT
+        };
+
+
+
+
+        for (int key: keys){
+            KeyboardEvent event = new KeyboardEvent();
+            event.setKey(key);
+            event.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+            keyboard.addEventListener(event);
+
+        }
+    }
+
+
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+
+        if(keyboardEvent.getKey() == KeyboardEvent.KEY_RIGHT){
+            client.moveRight();
+            return;
+        }
+
+        if(keyboardEvent.getKey() == KeyboardEvent.KEY_LEFT){
+            client.moveLeft();
+            return;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
+
+    }
 }
