c19bbe84b37d9f0ffbd74955e9d4f32d98257447
marianopicco <marianopicco@gmail.com>
2018-03-17 19:00:25 +0000

    added a class with public final static strings for file paths

---
+++ src/org/academiadecodigo/haltistas/besquare/FilePath.java
@@ -0,0 +1,14 @@
+package org.academiadecodigo.haltistas.besquare;
+
+public class FilePath {
+
+    public final static String PLAYER1 = "";
+    public final static String PLAYER2 = "";
+    public final static String TOKEN = "";
+    public final static String LEVEL_1 = "resources/assets/background.png";
+    public final static String LEVEL_2 = "";
+    public final static String LEVEL_3 = "";
+    public final static String LEVEL_4 = "";
+    public final static String LEVEL_5 = "";
+
+}
