3bcda63e5b2ca3668e6d1dee951e8bdcb6394f42
Pedro Catana <pedrocatana@sapo.pt>
2018-03-19 23:06:08 +0000

    update GameState with parameters to each state

--- src/org/academiadecodigo/haltistas/besquare/GameState.java
+++ src/org/academiadecodigo/haltistas/besquare/GameState.java
@@ -2,7 +2,18 @@ package org.academiadecodigo.haltistas.besquare;
 
 public enum GameState {
 
-    MENU,
-    GAME,
-    QUIT
+    MENU("M"),
+    GAME("G"),
+    NEW_GAME("N"),
+    QUIT("Q");
+
+    private String state;
+
+    GameState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
 }
