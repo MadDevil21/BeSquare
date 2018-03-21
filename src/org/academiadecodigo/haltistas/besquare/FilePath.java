56df712ad67b9816b388b073e165702a6b04c271
Bruno Santos <brunodominguessantos@gmail.com>
2018-03-21 00:39:54 +0000

    corrected level deign

--- src/org/academiadecodigo/haltistas/besquare/FilePath.java
+++ src/org/academiadecodigo/haltistas/besquare/FilePath.java
@@ -7,7 +7,7 @@ public class FilePath {
     public final static String TOKEN = "";
     public final static String LEVEL_1_PNG = "backgrounds/backgroundMockup_Level1_30x30.png";
     public static final String LEVEL_1_TXT = "resources/levels/level_1_tutorial.txt";
-    public final static String LEVEL_2_PNG = "backgrounds/backgroundMockup_Level1_30x30.png";
+    public final static String LEVEL_2_PNG = "backgrounds/backgroundMockup_Level2_30x30.png";
     public final static String LEVEL_2_TXT = "resources/levels/level_1_jumps.txt";
     public final static String LEVEL_3 = "";
     public final static String LEVEL_4 = "";
