ca88cf933d0c7a0e94ddaac8c772ab8ebd0f011e
Danlo <rubendamaia@gmail.com>
2018-03-23 11:03:00 +0000

    modified background and level

--- src/main/java/org/academiadecodigo/haltistas/besquare/FilePath.java
+++ src/main/java/org/academiadecodigo/haltistas/besquare/FilePath.java
@@ -7,25 +7,25 @@ public class FilePath {
     public final static String TOKEN = "sprites/token.png";
 
     public final static String LEVEL_1_PNG = "/backgrounds/level_1.png";
-    public final static String LEVEL_1_TXT = "/levels/level_1.txt";
+    public final static String LEVEL_1_TXT = "levels/level_1.txt";
 
     public final static String LEVEL_2_PNG = "/backgrounds/level_2.png";
-    public final static String LEVEL_2_TXT = "/levels/level_2.txt";
+    public final static String LEVEL_2_TXT = "levels/level_2.txt";
 
     public final static String LEVEL_3_PNG = "/backgrounds/level_3.png";
-    public final static String LEVEL_3_TXT = "/levels/level_3.txt";
+    public final static String LEVEL_3_TXT = "levels/level_3.txt";
 
     public final static String LEVEL_4_PNG = "/backgrounds/level_4.png";
-    public final static String LEVEL_4_TXT = "/levels/level_4.txt";
+    public final static String LEVEL_4_TXT = "levels/level_4.txt";
 
     public final static String LEVEL_5_PNG = "/backgrounds/level_5.png";
-    public final static String LEVEL_5_TXT = "/levels/level_5.txt";
+    public final static String LEVEL_5_TXT = "levels/level_5.txt";
 
     public final static String LEVEL_6_PNG = "/backgrounds/level_6";
-    public final static String LEVEL_6_TXT = "/levels/level_6.txt";
+    public final static String LEVEL_6_TXT = "levels/level_6.txt";
 
     public final static String LEVEL_7_PNG = "/backgrounds/endView.png";
-    public final static String LEVEL_7_TXT = "/levels/unbuilded_Level.txt";
+    public final static String LEVEL_7_TXT = "levels/unbuilded_Level.txt";
 
 
 
